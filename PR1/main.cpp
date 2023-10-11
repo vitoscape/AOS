53bf0f01d912297c8a9849ac2f546ed1605586aa
vitoscape <vit2001g@gmail.com>
2023-10-11 15:58:04 +0300

    Определены размеры карты и координаты агентов

--- PR1/main.cpp
+++ PR1/main.cpp
@@ -1,2 +1,16 @@
+#include <iostream>
+#include <queue>
 #include "main.hpp"
 
+using namespace std;
+
+
+class agent {
+    uint8_t coord[2]; // Координаты агента
+
+};
+
+
+int main() {
+    return 0;
+}
--- PR1/main.hpp
+++ PR1/main.hpp
@@ -1,4 +1,20 @@
 #ifndef main_hpp__
 #define main_hpp__
 
+/* Размеры карты */
+#define MAP_WIDTH 10    // Ширина карты
+#define MAP_HEIGHT 10   // Высота карты
+
+/* Начальные координаты агентов */
+#define A1_START {1, 1}     // Агент 1
+#define A2_START {3, 3}     // Агент 2
+#define A3_START {5, 5}     // Агент 3
+
+/* Конечные координаты агентов */
+#define A1_FINISH {2, 2}    // Агент 1
+#define A2_FINISH {4, 4}    // Агент 2
+#define A3_FINISH {6, 6}    // Агент 3
+
+
+
 #endif
