782b9019540fa340bf44e42aedaf4532a339f397
vitoscape <vit2001g@gmail.com>
2023-10-15 14:18:34 +0300

    Визуализация в сыром виде

--- PR1/main.cpp
+++ PR1/main.cpp
@@ -1,165 +1,111 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
 #include <vector>
+#include <iostream>
 #include <queue>
-#include <set>
-#include <algorithm>
-#include <cmath>
-#include <stdint.h>
-#include "main.hpp"
+#include <ctime>
 
-using namespace std;
+const int mapWidth = 10;
+const int mapHeight = 10;
+const int agentCount = 3;
+const int moveCost = 1;
 
+struct Node {
+    int x, y;
+    Node* parent;
+    int gCost;
+    int hCost;
 
+    Node(int x, int y, Node* parent, int gCost, int hCost)
+        : x(x), y(y), parent(parent), gCost(gCost), hCost(hCost) {}
 
-// Структура каждого узла карты
-struct node {
-    int x;          // Координаты узла
-    int y;
-    // int parentX;    // Родительские координаты
-    // int parentY;
-    float gCost;    // Стоимость пути от начального узла до данного узла
-    float hCost;    // Эвристическая оценка стоимости от этого узла до целевого
-    float fCost;    // Суммарная оценка fCost = gCost + hCost
-
-    node (int x, int y, int gCost, int hCost) : x(x), y(y), gCost(gCost), hCost(hCost), fCost(gCost + hCost) {}
+    int fCost() const {
+        return gCost + hCost;
+    }
 };
 
-/* Перегрузка оператора < для сравнения объектов структуры node.
- * Это нужно для того, чтобы объекты структуры node
- * можно было добавлять в set. 
- */
-inline bool operator < (const node& lhs, const node& rhs) {
-    return lhs.fCost < rhs.fCost;
+// Function to calculate Manhattan distance between two points
+int manhattanDistance(int x1, int y1, int x2, int y2) {
+    return abs(x1 - x2) + abs(y1 - y2);
 }
 
-
-
-// Карта (вектор векторов)
-std::vector<std::vector<int>> grid = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+struct CompareNodes {
+    bool operator()(const Node* lhs, const Node* rhs) const {
+        return lhs->fCost() > rhs->fCost();
+    }
 };
 
-// Направления движения агентов: вверх, вниз
-std::vector<std::pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+// A* pathfinding algorithm
+std::vector<std::pair<int, int>> findPath(int startX, int startY, int targetX, int targetY, std::vector<std::vector<int>>& map) {
+    std::vector<std::pair<int, int>> path;
+    std::priority_queue<Node*, std::vector<Node*>, CompareNodes> openSet;
 
-// Проверка на доступность узла
-bool isValid(int x, int y) {
-   return x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && grid[x][y] == 0;
-}
+    std::vector<std::vector<bool>> closedSet(mapWidth, std::vector<bool>(mapHeight, false));
 
-// Манхэттенское расстояние
-int heuristic(int x, int y, int targetX, int targetY) {
-    return std::abs(x - targetX) + std::abs(y - targetY);
-}
-
-
-std::vector<std::pair<int, int>> findPath(std::vector<std::vector<int>>& grid, int startX, int startY, int targetX, int targetY) {
-    std::priority_queue<node> openSet;
-    std::vector<std::vector<node>> nodes(grid.size(), std::vector<node>(grid[0].size(), node(0, 0, 0, 0)));
-
-    openSet.emplace(startX, startY, 0, heuristic(startX, startY, targetX, targetY));
-    nodes[startX][startY] = openSet.top();
+    Node* startNode = new Node(startX, startY, nullptr, 0, manhattanDistance(startX, startY, targetX, targetY));
+    openSet.push(startNode);
 
     while (!openSet.empty()) {
-        node current = openSet.top();
+        Node* currentNode = openSet.top();
         openSet.pop();
 
-        if (current.x == targetX && current.y == targetY) {
-            // Найден путь до цели
-            std::vector<std::pair<int, int>> path;
-            while (current.x != startX || current.y != startY) {
-                path.emplace_back(current.x, current.y);
-                int prevX = current.x - directions[0].first;
-                int prevY = current.y - directions[0].second;
-                current = nodes[prevX][prevY];
+        if (currentNode->x == targetX && currentNode->y == targetY) {
+            // Found the target, reconstruct the path
+            while (currentNode != nullptr) {
+                path.push_back({currentNode->x, currentNode->y});
+                currentNode = currentNode->parent;
             }
-            path.emplace_back(startX, startY);
-            std::reverse(path.begin(), path.end());
-            return path;
+            break;
         }
 
-        for (const auto& direction : directions) {
-            int newX = current.x + direction.first;
-            int newY = current.y + direction.second;
-            if (isValid(newX, newY)) {
-                int tentativeG = current.gCost + 1;
-                if (tentativeG < nodes[newX][newY].gCost || nodes[newX][newY].gCost == 0) {
-                    nodes[newX][newY] = {newX, newY, tentativeG, heuristic(newX, newY, targetX, targetY)};
-                    openSet.emplace(newX, newY, nodes[newX][newY].gCost, nodes[newX][newY].hCost);
-                }
+        closedSet[currentNode->x][currentNode->y] = true;
+
+        // Generate neighboring nodes
+        std::vector<std::pair<int, int>> neighbors = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (const auto& neighbor : neighbors) {
+            int neighborX = currentNode->x + neighbor.first;
+            int neighborY = currentNode->y + neighbor.second;
+
+            // Check if the neighbor is valid
+            if (neighborX >= 0 && neighborX < mapWidth && neighborY >= 0 && neighborY < mapHeight
+                && map[neighborX][neighborY] == 0 && !closedSet[neighborX][neighborY]) {
+                int gCost = currentNode->gCost + moveCost;
+                int hCost = manhattanDistance(neighborX, neighborY, targetX, targetY);
+                Node* neighborNode = new Node(neighborX, neighborY, currentNode, gCost, hCost);
+                openSet.push(neighborNode);
             }
         }
     }
 
-    return {}; // Путь не найден
-}
-
-void drawPath(sf::RenderWindow& window, const std::vector<std::pair<int, int>>& path) {
-    sf::VertexArray lines(sf::LinesStrip, path.size());
-    for (size_t i = 0; i < path.size(); ++i) {
-        lines[i].position = sf::Vector2f(path[i].first * 50 + 25, path[i].second * 50 + 25);
-        lines[i].color = sf::Color::Red;
+    // Clean up allocated nodes
+    while (!openSet.empty()) {
+        delete openSet.top();
+        openSet.pop();
     }
-    window.draw(lines);
+
+    return path;
 }
-
-// /////////////////////////////////////////////////////////////////////////////////// Координаты агентов ///////////////////////////////////////////////////////////////
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// /* Координаты агентов задаются в виде массива из двух элементов:
-//  * 0-й элемент — координата X,
-//  * 1-й элемент — координата Y.
-//  */
-// /* Начальные координаты агентов */
-// static const uint8_t A1_START[2] = {1, 1};     // Агент 1
-// static const uint8_t A2_START[2] = {3, 3};     // Агент 2
-// static const uint8_t A3_START[2] = {5, 5};     // Агент 3
-
-// /* Конечные координаты агентов */
-// static const uint8_t A1_FINISH[2] = {2, 2};    // Агент 1
-// static const uint8_t A2_FINISH[2] = {4, 4};    // Агент 2
-// static const uint8_t A3_FINISH[2] = {6, 6};    // Агент 3
-
-
-
-/*
-// Класс агента
-class agent {
-    uint8_t start_x;    // Начальные координаты агента
-    uint8_t start_y;
-    uint8_t finish_x;   // Конечные координаты агента
-    uint8_t finish_y;
-    uint8_t x;          // Координаты агента
-    uint8_t y;
-
-public:
-    agent(uint8_t st_x, uint8_t st_y, uint8_t fin_x, uint8_t fin_y) {
-        start_x = st_x;
-        start_y = st_y;
-        finish_x = fin_x;
-        finish_y = fin_y;
-    }
-};
-*/
-
-
 
 int main() {
-    int startX = 0, startY = 0;
-    int targetX = 4, targetY = 4;
+    // Initialize SFML window
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Multi-Agent Pathfinding");
 
-    std::vector<std::pair<int, int>> path = findPath(grid, startX, startY, targetX, targetY);
+    // Define your initial map, agent positions, and target positions here
+    std::vector<std::vector<int>> map(mapWidth, std::vector<int>(mapHeight, 0));
 
-    sf::RenderWindow window(sf::VideoMode(250, 250), "A* Pathfinding Visualization");
+    // Initialize SFML shapes and colors for visualization
+    sf::CircleShape agentShapes[agentCount];
+    sf::CircleShape targetShapes[agentCount];
+    sf::Color agentColors[agentCount] = {sf::Color::Red, sf::Color::Green, sf::Color::Blue};
+
+    for (int i = 0; i < agentCount; ++i) {
+        agentShapes[i] = sf::CircleShape(20);
+        agentShapes[i].setFillColor(agentColors[i]);
+
+        targetShapes[i] = sf::CircleShape(20);
+        targetShapes[i].setFillColor(sf::Color::Yellow);
+    }
+
+    // Define your main loop for visualization and interaction here
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -167,18 +113,51 @@ int main() {
                 window.close();
             }
         }
-        window.clear(sf::Color::White);
-        drawPath(window, path);
-        window.display(); 
+
+        window.clear();
+
+        // Draw your map
+        sf::RectangleShape tile(sf::Vector2f(40, 40));
+        for (int i = 0; i < mapWidth; ++i) {
+            for (int j = 0; j < mapHeight; ++j) {
+                if (map[i][j] == 1) {
+                    tile.setFillColor(sf::Color::Black);
+                } else {
+                    tile.setFillColor(sf::Color::White);
+                }
+                tile.setPosition(i * 40, j * 40);
+                window.draw(tile);
+            }
+        }
+
+        // Draw the agents and their paths
+        int startX[agentCount] = {2, 5, 8};
+        int startY[agentCount] = {2, 5, 8};
+        int targetX[agentCount] = {7, 2, 5};
+        int targetY[agentCount] = {7, 4, 1};
+
+        for (int i = 0; i < agentCount; ++i) {
+            // Draw the path
+            auto path = findPath(startX[i], startY[i], targetX[i], targetY[i], map);
+            for (auto& node : path) {
+                sf::RectangleShape pathTile(sf::Vector2f(40, 40));
+                pathTile.setFillColor(sf::Color::Cyan);
+                pathTile.setPosition(node.first * 40, node.second * 40);
+                window.draw(pathTile);
+            }
+
+            // Draw the agent
+            agentShapes[i].setPosition(startX[i] * 40, startY[i] * 40);
+            window.draw(agentShapes[i]);
+
+            // Draw the target
+            targetShapes[i].setPosition(targetX[i] * 40, targetY[i] * 40);
+            window.draw(targetShapes[i]);
+        }
+
+        window.display();
+
     }
-    
-/*
-    // Создание агентов
-    agent Agent1(A1_START[0], A1_START[1], A1_FINISH[0], A1_FINISH[1]);
-    agent Agent2(A2_START[0], A2_START[1], A2_FINISH[0], A2_FINISH[1]);
-    agent Agent3(A3_START[0], A3_START[1], A3_FINISH[0], A3_FINISH[1]);
-*/
-    
 
     return 0;
 }
